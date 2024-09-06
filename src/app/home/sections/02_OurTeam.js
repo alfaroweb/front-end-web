0394122d2a459f085eede75207f043ca37cfbf77
Alberto Sánchez <albersanchez511@hotmail.com>
2024-09-06 14:12:31 +0200

    fix doctors-bro import

--- src/app/home/sections/02_OurTeam.js
+++ src/app/home/sections/02_OurTeam.js
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import DoctorsGift from '/public/specialities/doctors-bro.png'
+import DoctorsGift from '/public/specialities/Doctors-bro.png'
 import Waves from '/public/home/svg.png'
 
 function OurTeam() {
