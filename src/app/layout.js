f1daafeab69b36e20afdb8f6e70fe7a7b8c624b3
alfaroweb <web.cirugiadelamanoalbacete@gmail.com>
2024-10-16 12:32:47 +0200

    modify google tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -63,6 +63,7 @@ import { Montserrat } from 'next/font/google'
 import './globals.css'
 import Header from './ui/Header'
 import Footer from './ui/Footer'
+import { GoogleTagManager } from '@next/third-parties/google'
 import Script from 'next/script'
 import { Partytown } from '@builder.io/partytown/react'
 
@@ -77,7 +78,6 @@ export default function RootLayout({ children }) {
         <Header />
         {children}
         <Footer />
-
         {/* Cookiebot script */}
         <Script
           id='Cookiebot'
@@ -88,55 +88,8 @@ export default function RootLayout({ children }) {
           strategy='beforeInteractive'
           async={true}
         />
-
-        {/* Google Tag Manager script using Partytown */}
-        <Script
-          id='GTM'
-          type='text/partytown'
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i) {
-                w[l]=w[l]||[]; 
-                w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
-                var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),
-                dl=l!='dataLayer'?'&l='+l:'';
-                j.async=true;
-                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-                f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-TZPMSMV5');
-            `
-          }}
-        />
-
-        {/* Partytown setup to forward dataLayer.push */}
-        <Partytown forward={['dataLayer.push']} />
-
-        {/* Noscript fallback for GTM */}
-        <noscript>
-          <iframe
-            src='https://www.googletagmanager.com/ns.html?id=GTM-TZPMSMV5'
-            height='0'
-            width='0'
-            style={{ display: 'none', visibility: 'hidden' }}
-          ></iframe>
-        </noscript>
-
-        {/* Escuchar el evento de consentimiento */}
-        <Script
-          id='Consent Listener'
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.addEventListener('CookieConsentDeclaration', function() {
-                if (Cookiebot.consent.statistics) {
-                  window.dataLayer = window.dataLayer || [];
-                  window.dataLayer.push({'event': 'cookieConsentGranted'});
-                }
-              });
-            `
-          }}
-        />
       </body>
+      <GoogleTagManager gtmId='GTM-TZPMSMV5' />
     </html>
   )
 }
