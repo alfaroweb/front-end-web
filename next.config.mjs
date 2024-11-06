1bcb50c1cc969381246a68791645d378ec2844e5
alfaroweb <web.cirugiadelamanoalbacete@gmail.com>
2024-11-06 09:41:23 +0100

    Fix mobile menú

--- next.config.mjs
+++ next.config.mjs
@@ -14,36 +14,36 @@ const nextConfig = {
         hostname: 'res.cloudinary.com'
       }
     ]
-  },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: "default-src 'self';"
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'DENY'
-          },
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff'
-          },
-          {
-            key: 'Referrer-Policy',
-            value: 'no-referrer'
-          },
-          {
-            key: 'Permissions-Policy',
-            value: 'geolocation=(self), microphone=()'
-          }
-        ]
-      }
-    ]
   }
+  // async headers() {
+  //   return [
+  //     {
+  //       source: '/(.*)',
+  //       headers: [
+  //         {
+  //           key: 'Content-Security-Policy',
+  //           value: "default-src 'self';"
+  //         },
+  //         {
+  //           key: 'X-Frame-Options',
+  //           value: 'DENY'
+  //         },
+  //         {
+  //           key: 'X-Content-Type-Options',
+  //           value: 'nosniff'
+  //         },
+  //         {
+  //           key: 'Referrer-Policy',
+  //           value: 'no-referrer'
+  //         },
+  //         {
+  //           key: 'Permissions-Policy',
+  //           value: 'geolocation=(self), microphone=()'
+  //         }
+  //       ]
+  //     }
+  //   ]
+  // }
 }
 
 export default nextConfig
