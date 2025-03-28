dc40e2cc1786868487fe4efaa08cb2a74f77c7ab
alfaroweb <web.cirugiadelamanoalbacete@gmail.com>
2025-03-28 13:06:18 +0100

    "quitar post"

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -2,7 +2,7 @@ import { fetchLatestPosts } from '../utils/fetch/fetchLatestPosts'
 import PaginationPosts from './components/PaginationPosts'
 
 async function page() {
-  const latestPosts = await fetchLatestPosts()
+  // const latestPosts = await fetchLatestPosts()
 
   return (
     <main className='mx-auto w-[90%] max-w-screen-lg py-[50px]'>
